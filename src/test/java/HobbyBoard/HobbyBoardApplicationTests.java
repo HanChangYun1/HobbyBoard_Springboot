587769857c7343abd0e8ec4e19290092cb45938a
HanChangYun1 <hansyooni11@gmail.com>
2024-01-26 18:00:00 +0900

    Initial commit

---
+++ src/test/java/HobbyBoard/HobbyBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package HobbyBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbyBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
